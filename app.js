58a08ad70a11fe2a21cd3681200c23c48a8e08c0
Sairam Muthyala <s567075@nwmissouri.edu>
2023-11-01 23:27:25 -0500

    board pug

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EntertainmentRouter = require('./routes/entertainment');
+var BoardRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/entertainment', EntertainmentRouter);
+app.use('/board', BoardRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
